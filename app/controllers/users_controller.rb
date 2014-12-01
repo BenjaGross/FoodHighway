ccfb36cd139dbb4b6d020a72dc61250d63700d43
changamanda <amanda.chang10@gmail.com>
2014-12-01 17:07:57 -0500

    Add New User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
+  def index
+  	@users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
-  def index
+  def new
+  	@user = User.new
   end
+
+  def create
+  	new_user = User.create(user_params)
+  	redirect_to new_user
+  end
+
+  private
+
+  	def user_params
+  		params.require(:user).permit(:first_name, :last_name, :email, :password)
+  	end
 end
