789160b65d74b3a229fa3cd7f8c90bfd8ce66620
mhfairbank <mhfairbank@gmail.com>
2014-12-01 16:02:31 -0500

    build food methods

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,8 @@
 class Team < ActiveRecord::Base
   has_many :user_teams
   has_many :users, through: :user_teams
+
+  def foods
+    self.user_teams.map{|ut| ut.foods}.flatten
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name, :email, :password, presence: true
   has_many :user_teams
   has_many :teams, through: :user_teams
+
+  def all_foods
+    self.user_teams.map{|ut| ut.foods}.flatten
+  end
+
+  def foods(team_id)
+    self.user_teams.where(team_id: team_id)[0].foods
+  end
 end
