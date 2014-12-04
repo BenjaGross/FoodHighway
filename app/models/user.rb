b25b2445ecff56bc8ce9807a10dc3bb8b68fd286
mhfairbank <mhfairbank@gmail.com>
2014-12-04 15:03:37 -0500

    decided scoping was not appropriate for needs, branch abandoned

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,15 @@ class User < ActiveRecord::Base
   # Extra Devise Validation
   has_many :user_teams
   has_many :teams, through: :user_teams
+  has_many :foods, through: :user_teams
 
   def all_foods
-    self.user_teams.map{|ut| ut.foods}.flatten
+    self.foods.unscoped
   end
 
-  def foods(team_id)
-    self.user_teams.where(team_id: team_id)[0].foods
-  end
+  # def foods(team_id)
+  #   self.user_teams.where(team_id: team_id)[0].foods
+  # end
 
   def full_name
     "#{self.first_name} #{self.last_name}"
