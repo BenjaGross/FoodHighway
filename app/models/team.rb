e435b7aa7fc1eb69562f7425670beb1ed7e432b7
mhfairbank <mhfairbank@gmail.com>
2014-12-04 15:09:52 -0500

    build team food association directly

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,10 +1,7 @@
 class Team < ActiveRecord::Base
   has_many :user_teams
   has_many :users, through: :user_teams
-
-  def foods
-    self.user_teams.map{|ut| ut.foods}.flatten
-  end
+  has_many :foods, through: :user_teams
 
   def current_weight
   	current = self.foods.map{|food| food.weight}.inject(:+)
