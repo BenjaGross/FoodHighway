b6157839000ef1c68628fdb565254c1ce11137e8
mhfairbank <mhfairbank@gmail.com>
2014-12-05 10:11:58 -0500

    make userteam creation find or create by

--- app/controllers/user_teams_controller.rb
+++ app/controllers/user_teams_controller.rb
@@ -1,6 +1,6 @@
 class UserTeamsController < ApplicationController
   def create
-  	UserTeam.create(team_id: params[:user_team][:team_id], user_id: current_user.id)
+  	UserTeam.find_or_create_by(team_id: params[:user_team][:team_id], user_id: current_user.id)
     redirect_to current_user
   end
 
